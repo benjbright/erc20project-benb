2b9a179a4ec2cc84113dc4dc8b52be1fb97392bd
Ben <105577567+benjbright@users.noreply.github.com>
2022-07-30 13:35:45 +0100

    Update Token.js

--- test/Token.js
+++ test/Token.js
@@ -1,12 +1,25 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 const { expect } = require("chai")
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners()
-
+  async function deployTokenFixture() {
     const Token = await ethers.getContractFactory("Token")
+    const [owner, addr1, addr2] = await ethers.getSigners()
 
     const token = await Token.deploy()
+    await token.deployed()
+    console.log(`Token deployed to: ${token.address}`)
+
+    return { Token, token, owner, addr1, addr2 }
+  }
+
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    // const [owner] = await ethers.getSigners()
+
+    // const Token = await ethers.getContractFactory("Token")
+
+    // const token = await Token.deploy()
+    const { token, owner } = await loadFixture(deployTokenFixture)
 
     const ownerBalance = await token.balanceOf(owner.address)
 
@@ -14,11 +27,12 @@ describe("Token contract", function () {
   })
 
   it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners()
+    // const [owner, addr1, addr2] = await ethers.getSigners()
 
-    const Token = await ethers.getContractFactory("Token")
+    // const Token = await ethers.getContractFactory("Token")
 
-    const token = await Token.deploy()
+    // const token = await Token.deploy()
+    const { token, owner, addr1, addr2 } = await loadFixture(deployTokenFixture)
 
     // Transfer tokens from owner to addr1
     await token.transfer(addr1.address, 100000)
